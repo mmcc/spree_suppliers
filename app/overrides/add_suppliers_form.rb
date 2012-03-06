00c55e24907a9aa609992df3a7fb8667a5bc1bee
Matt McClure <matt@htbx.net>
2012-03-06 00:26:57 -0500

    trying to get the override to work. pushing disabled version

--- app/overrides/add_suppliers_form.rb
+++ app/overrides/add_suppliers_form.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/_form.html.erb",
+Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_suppliers_form",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<p>
